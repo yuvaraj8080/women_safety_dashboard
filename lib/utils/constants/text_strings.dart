2fc8490645670df557e7cca78419453cd9348274
Yuvaraj Dekhane <148527838+yuvaraj8080@users.noreply.github.com>
2025-02-06 19:47:38 +0530

    Commit for the remove all errors and make correct basin structure

--- lib/utils/constants/text_strings.dart
+++ lib/utils/constants/text_strings.dart
@@ -18,18 +18,9 @@ class TTexts {
   static const String skip = "Skip";
   static const String done = "Done";
   static const String submit = "Submit";
-  static const String appName = "Mahakal";
+  static const String appName = "SheShield:Women Safety Dashboard";
   static const String tContinue = "Continue";
 
-
-  // -- OnBoarding Texts
-  static const String onBoardingTitle1 = "Choose your product";
-  static const String onBoardingTitle2 = "Select Payment Method";
-  static const String onBoardingTitle3 = "Deliver at your door step";
-
-  static const String onBoardingSubTitle1 = "Welcome to a World of Limitless Choices - Your Perfect Product Awaits!";
-  static const String onBoardingSubTitle2 = "For Seamless Transactions, Choose Your Payment Path - Your Convenience, Our Priority!";
-  static const String onBoardingSubTitle3 = "From Our Doorstep to Yours - Swift, Secure, and Contactless Delivery!";
 
 
   // -- Authentication Forms
