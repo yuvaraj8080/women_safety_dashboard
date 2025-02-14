5d0b892e19c1b9d3348f326c7b23265a0a496adc
Yuvaraj Dekhane <148527838+yuvaraj8080@users.noreply.github.com>
2025-02-14 10:28:45 +0530

    this is the new update for the google map

--- lib/common/widgets/layouts/sidebars/sidebar.dart
+++ lib/common/widgets/layouts/sidebars/sidebar.dart
@@ -41,6 +41,7 @@ class TSidebar extends StatelessWidget {
 
                         ])
                 )
+
               ],
             )
         ),
--- lib/features/dashboard/responsive_screen/dashboard_desktop.dart
+++ lib/features/dashboard/responsive_screen/dashboard_desktop.dart
@@ -12,94 +12,94 @@ class DashboardDesktopScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        // body: SingleChildScrollView(
-        //   child: Padding(
-        //       padding: const EdgeInsets.all(TSizes.spaceBtwItems16),
-        //       child: Column(
-        //         crossAxisAlignment: CrossAxisAlignment.start,
-        //         children: [
-        //
-        //           /// HEADING
-        //           Text("Dashboard", style: Theme.of(context).textTheme
-        //               .headlineMedium),
-        //           const SizedBox(height: TSizes.sm8),
-        //
-        //           /// CARDS
-        //              Row(
-        //                 children: [
-        //                   Expanded(child: TDashboardCard(
-        //                       title: 'Total Crime', subTitle:"5000")),
-        //                   const SizedBox(width: TSizes.ms12),
-        //                    Expanded(child: TDashboardCard(
-        //                       title: 'Solved Crime', subTitle:"159")),
-        //                   const SizedBox(width: TSizes.ms12),
-        //                   Expanded(child: TDashboardCard(
-        //                       title: 'Unsolved Crime', subTitle:"57")),
-        //                   const SizedBox(width: TSizes.ms12),
-        //                   Expanded(child: TDashboardCard(
-        //                       title: 'Pending Crime',subTitle: "470")),
-        //                 ]),
-        //           const SizedBox(height: TSizes.ms12),
-        //
-        //           // / GRAPH IN THE DASHBOARD
-        //           Row(
-        //               crossAxisAlignment: CrossAxisAlignment.start,
-        //               children: [
-        //                 Expanded(flex: 2,
-        //                     child: Column(
-        //                         children: [
-        //                           TRoundedContainer(
-        //                             child:Column(
-        //                               children:[
-        //
-        //                                 //// CRIME TREND HEADER AND CRIME TYPE SELECT BUTTON  ////
-        //                                 Row(
-        //                                   mainAxisAlignment:MainAxisAlignment.spaceBetween,
-        //                                   children: [
-        //                                     Text("Crime Trends",style:Theme.of(context).textTheme.headlineSmall),
-        //                                     OutlinedButton(onPressed:(){}, child:Text("Harassment"))
-        //                                   ],
-        //                                 ),
-        //                                 /// CRIME TREND GRAPH
-        //                                 const YearlyCrimeReport(),
-        //
-        //                               ],
-        //                             ),
-        //                           ),
-        //
-        //                           /// ORDERS
-        //                           // TRoundedContainer(
-        //                           //     child: Column(
-        //                           //         crossAxisAlignment: CrossAxisAlignment
-        //                           //             .start,
-        //                           //         children: [
-        //                           //           Row(
-        //                           //             mainAxisAlignment:MainAxisAlignment.spaceBetween ,
-        //                           //             children: [
-        //                           //               Text("Recent 20 Orders", style: Theme.of(context).textTheme.headlineSmall),
-        //                           //               OutlinedButton(onPressed:()=> Get.offNamed(TRoutes.orders),
-        //                           //                   child:const Text("View All Orders"))
-        //                           //             ],
-        //                           //           ), const SizedBox(height: TSizes.xs4),
-        //                           //
-        //                           //           Obx(() {
-        //                           //             if(orderController.isLoading.value) return const TLoaderAnimation();
-        //                           //             return const DashboardOrderTable();
-        //                           //           }),
-        //                           //         ])
-        //                           // ),
-        //                         ]
-        //                     )),
-        //                 const SizedBox(width: TSizes.sm8),
-        //
-        //                 /// PIE CHART
-        //                 // const Expanded(child: OrderStatusPieChart())
-        //               ]
-        //           )
-        //         ],
-        //       )
-        //   ),
-        // )
+        body: SingleChildScrollView(
+          child: Padding(
+              padding: const EdgeInsets.all(TSizes.spaceBtwItems16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+
+                  /// HEADING
+                  Text("Dashboard", style: Theme.of(context).textTheme
+                      .headlineMedium),
+                  const SizedBox(height: TSizes.sm8),
+
+                  /// CARDS
+                     Row(
+                        children: [
+                          Expanded(child: TDashboardCard(
+                              title: 'Total Crime', subTitle:"5000")),
+                          const SizedBox(width: TSizes.ms12),
+                           Expanded(child: TDashboardCard(
+                              title: 'Solved Crime', subTitle:"159")),
+                          const SizedBox(width: TSizes.ms12),
+                          Expanded(child: TDashboardCard(
+                              title: 'Unsolved Crime', subTitle:"57")),
+                          const SizedBox(width: TSizes.ms12),
+                          Expanded(child: TDashboardCard(
+                              title: 'Pending Crime',subTitle: "470")),
+                        ]),
+                  const SizedBox(height: TSizes.ms12),
+
+                  // / GRAPH IN THE DASHBOARD
+                  Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Expanded(flex: 2,
+                            child: Column(
+                                children: [
+                                  TRoundedContainer(
+                                    child:Column(
+                                      children:[
+
+                                        //// CRIME TREND HEADER AND CRIME TYPE SELECT BUTTON  ////
+                                        Row(
+                                          mainAxisAlignment:MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Text("Crime Trends",style:Theme.of(context).textTheme.headlineSmall),
+                                            OutlinedButton(onPressed:(){}, child:Text("Harassment"))
+                                          ],
+                                        ),
+                                        /// CRIME TREND GRAPH
+                                        const YearlyCrimeReport(),
+
+                                      ],
+                                    ),
+                                  ),
+
+                                  /// ORDERS
+                                  // TRoundedContainer(
+                                  //     child: Column(
+                                  //         crossAxisAlignment: CrossAxisAlignment
+                                  //             .start,
+                                  //         children: [
+                                  //           Row(
+                                  //             mainAxisAlignment:MainAxisAlignment.spaceBetween ,
+                                  //             children: [
+                                  //               Text("Recent 20 Orders", style: Theme.of(context).textTheme.headlineSmall),
+                                  //               OutlinedButton(onPressed:()=> Get.offNamed(TRoutes.orders),
+                                  //                   child:const Text("View All Orders"))
+                                  //             ],
+                                  //           ), const SizedBox(height: TSizes.xs4),
+                                  //
+                                  //           Obx(() {
+                                  //             if(orderController.isLoading.value) return const TLoaderAnimation();
+                                  //             return const DashboardOrderTable();
+                                  //           }),
+                                  //         ])
+                                  // ),
+                                ]
+                            )),
+                        const SizedBox(width: TSizes.sm8),
+
+                        /// PIE CHART
+                        // const Expanded(child: OrderStatusPieChart())
+                      ]
+                  )
+                ],
+              )
+          ),
+        )
     );
   }
 }
